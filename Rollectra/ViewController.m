a32c376282a9804fb9faf92394939bc97870f927
Pwn20wnd <pwn20wndstuff@gmail.com>
2018-08-31 21:01:01 +0300

    one more thing

--- Rollectra/ViewController.m
+++ Rollectra/ViewController.m
@@ -335,20 +335,6 @@ void unjailbreak(mach_port_t tfp0, uint64_t kernel_base, int shouldEraseUserData
     [[UIApplication sharedApplication] openURL:[ViewController getURLForUserName:@"aesign_"] options:@{} completionHandler:nil];
 }
 
-#if 0
-
-Chariz Description:
--------------------
-Using this tool will restore the RootFS to its stock state and reset the user data.
-So creating a backup using iTunes before using it is recommended.
-It supports all devices running iOS 11.3 to 11.4 Beta 3.
-The 11.2 to 11.2.6 support will be coming really soon.
-The 11.0 to 11.1.2 support will also be added in the future, so keep checking this package if you are interested in that.
-Although there's an option to not perform a full restore and keep the user data, I would really not recommend it, since it will leave a lot of left-overs from your jailbreak.
-Follow me at @Pwn20wnd for future updates about this tool or to troubleshoot any unexpected behavior that you may be having with this tool.
-
-#endif
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
