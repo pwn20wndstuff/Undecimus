a4f0bfa632d34a0af8ebc09ea956d1363e0deeda
Sam Bingner <sam@bingner.com>
2018-10-04 09:55:08 -1000

    Use sharedController objc singleton instead of global variable directly and make _assert available

--- Undecimus/ViewController.h
+++ Undecimus/ViewController.h
@@ -8,8 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
+#define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define _assert(test) do \
+    if (!(test)) { \
+        fprintf(stderr, "__assert(%d:%s)@%s:%u[%s]\n", errno, #test, __FILENAME__, __LINE__, __FUNCTION__); \
+        dispatch_semaphore_t semaphore; \
+        semaphore = dispatch_semaphore_create(0); \
+        dispatch_async(dispatch_get_main_queue(), ^{ \
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error" message:[NSString stringWithFormat:@"Errno: %d\nTest: %s\nFilename: %s\nLine: %d\nFunction: %s", errno, #test, __FILENAME__, __LINE__, __FUNCTION__] preferredStyle:UIAlertControllerStyleAlert]; \
+            UIAlertAction *OK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { \
+                dispatch_semaphore_signal(semaphore); \
+            }]; \
+            [alertController addAction:OK]; \
+            [alertController setPreferredAction:OK]; \
+            [[[[[UIApplication sharedApplication] delegate] window] rootViewController] presentViewController:alertController animated:YES completion:nil];; \
+        }); \
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER); \
+        exit(1); \
+    } \
+while (false)
+
 @interface ViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UIButton *goButton;
 
++(ViewController*)sharedController;
 @end
 
