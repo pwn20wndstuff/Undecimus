62600bde309d8a28e5945dfa384388b5cb25cd9d
Sam Bingner <sam@bingner.com>
2018-10-08 12:53:25 -1000

    Update bootstrap and stop creating "available" file since it's properly in the bootstrap now

--- Undecimus/ViewController.m
+++ Undecimus/ViewController.m
@@ -1820,18 +1820,6 @@ void exploit(mach_port_t tfp0, uint64_t kernel_base, int load_tweaks, int load_d
             rv = execCommandAndWait("/jb/tar", "--use-compress-program=/jb/lzma", "-xvpkf", "/var/tmp/strap.tar.lzma", NULL, NULL);
             LOG("rv: " "%d" "\n", rv);
             _assert(rv == 512 || rv == 0);
-            a = fopen("/var/lib/dpkg/available", "w");
-            LOG("a: " "%p" "\n", a);
-            _assert(a != NULL);
-            rv = fclose(a);
-            LOG("rv: " "%d" "\n", rv);
-            _assert(rv == 0);
-            rv = chmod("/var/lib/dpkg/available", 0644);
-            LOG("rv: " "%d" "\n", rv);
-            _assert(rv == 0);
-            rv = chown("/var/lib/dpkg/available", 0, 0);
-            LOG("rv: " "%d" "\n", rv);
-            _assert(rv == 0);
             a = fopen("/.installed_unc0ver", "w");
             LOG("a: " "%p" "\n", a);
             _assert(a != NULL);
