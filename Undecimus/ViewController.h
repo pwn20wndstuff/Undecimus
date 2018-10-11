6442d871fb8b8fff185ae6acc078ad31982bf34e
Pwn20wnd <pwn20wndstuff@gmail.com>
2018-10-11 22:23:39 +0300

    Code improvements

--- Undecimus/ViewController.h
+++ Undecimus/ViewController.h
@@ -13,41 +13,31 @@
 #define _assert(test) do \
     if (!(test)) { \
         fprintf(stderr, "__assert(%d:%s)@%s:%u[%s]\n", errno, #test, __FILENAME__, __LINE__, __FUNCTION__); \
-        dispatch_semaphore_t semaphore; \
-        semaphore = dispatch_semaphore_create(0); \
-        dispatch_async(dispatch_get_main_queue(), ^{ \
-            [[ViewController sharedController] dismissViewControllerAnimated:YES completion:nil]; \
-            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error" message:[NSString stringWithFormat:@"Errno: %d\nTest: %s\nFilename: %s\nLine: %d\nFunction: %s", errno, #test, __FILENAME__, __LINE__, __FUNCTION__] preferredStyle:UIAlertControllerStyleAlert]; \
-            UIAlertAction *OK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { \
-                dispatch_semaphore_signal(semaphore); \
-            }]; \
-            [alertController addAction:OK]; \
-            [alertController setPreferredAction:OK]; \
-            [[[[[UIApplication sharedApplication] delegate] window] rootViewController] presentViewController:alertController animated:YES completion:nil];; \
-        }); \
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER); \
+        showAlert(@"Error", [NSString stringWithFormat:@"Errno: %d\nTest: %s\nFilename: %s\nLine: %d\nFunction: %s", errno, #test, __FILENAME__, __LINE__, __FUNCTION__], 1); \
         exit(1); \
     } \
 while (false)
 
-#define NOTICE(msg, wait) do { \
-    dispatch_semaphore_t semaphore; \
-    if (wait) \
-        semaphore = dispatch_semaphore_create(0); \
-    dispatch_async(dispatch_get_main_queue(), ^{ \
-        [[ViewController sharedController] dismissViewControllerAnimated:YES completion:nil]; \
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Notice" message:@(msg) preferredStyle:UIAlertControllerStyleAlert]; \
-        UIAlertAction *OK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { \
-            if (wait) \
-                dispatch_semaphore_signal(semaphore); \
-        }]; \
-        [alertController addAction:OK]; \
-        [alertController setPreferredAction:OK]; \
-        [[[[[UIApplication sharedApplication] delegate] window] rootViewController] presentViewController:alertController animated:YES completion:nil];; \
-    }); \
-    if (wait) \
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER); \
-} while (false)
+#define NOTICE(msg, wait) showAlert(@"Notice", @(msg), wait)
+
+static inline void showAlert(NSString *title, NSString *message, Boolean wait) {
+    dispatch_semaphore_t semaphore;
+    if (wait)
+        semaphore = dispatch_semaphore_create(0);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[[[[UIApplication sharedApplication] delegate] window] rootViewController] dismissViewControllerAnimated:YES completion:nil];
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *OK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            if (wait)
+                dispatch_semaphore_signal(semaphore);
+        }];
+        [alertController addAction:OK];
+        [alertController setPreferredAction:OK];
+        [[[[[UIApplication sharedApplication] delegate] window] rootViewController] presentViewController:alertController animated:YES completion:nil];;
+    });
+    if (wait)
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+}
 
 @interface ViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UIButton *goButton;
