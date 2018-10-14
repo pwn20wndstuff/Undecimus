8bab19c6b05fb964ad42b135a37da75b821764c3
Pwn20wnd <pwn20wndstuff@gmail.com>
2018-10-14 15:15:39 +0300

    Add a label to display the uptime, remove the custom fonts as they are the same with the system fonts, enable logging again and include spawn in the PATH

--- Undecimus/main.m
+++ Undecimus/main.m
@@ -14,6 +14,7 @@
 int (*_system)(const char *) = 0;
 
 int main(int argc, char * argv[]) {
+    START_LOGGING();
     @autoreleasepool {
         _system = dlsym(RTLD_DEFAULT,"system");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
