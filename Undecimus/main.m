b4a975937bb8c482052ee05b64c93c538d1d0dd1
Pwn20wnd <pwn20wndstuff@gmail.com>
2018-10-14 19:51:54 +0300

    Enable logging again

--- Undecimus/main.m
+++ Undecimus/main.m
@@ -15,6 +15,7 @@ int (*_system)(const char *) = 0;
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        START_LOGGING();
         _system = dlsym(RTLD_DEFAULT,"system");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
