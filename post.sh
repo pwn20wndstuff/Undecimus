9a188104623ff9322a8cbaf9c6c00fac6d582afa
Sam Bingner <sam@bingner.com>
2019-01-13 13:08:36 -1000

    Automatically generate unc0ver version from git tags

--- post.sh
+++ post.sh
@@ -12,4 +12,14 @@ if [ -z "${RESOURCES_VERSION}" ]; then
 else
     echo "Bundled resources: ${RESOURCES_VERSION}"
 fi
+
+PACKAGE_VERSION="$(git describe --tags --match="v*" | sed -e 's@-\([^-]*\)-\([^-]*\)$@+\1.\2@;s@^v@@;s@%@~@g')"
+if [ -z "${PACKAGE_VERSION}" ]; then
+    echo "Could not generate package version"
+    exit 1
+else
+    echo "Package Version: ${PACKAGE_VERSION}"
+fi
+
 defaults write "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}" BundledResources "${RESOURCES_VERSION}"
+defaults write "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}" CFBundleShortVersionString "${PACKAGE_VERSION}"
