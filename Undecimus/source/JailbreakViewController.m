d40946f5e584f22d21a249d965b07b6686f29cd3
Sam Bingner <sam@bingner.com>
2019-01-25 13:27:29 -1000

    Fix up text display window

--- Undecimus/source/JailbreakViewController.m
+++ Undecimus/source/JailbreakViewController.m
@@ -1973,12 +1973,12 @@ void exploit()
 
 -(void)appendTextToOutput:(NSString *)text {
     static NSRegularExpression *remove = nil;
-    if (remove == nil)
-        remove = [NSRegularExpression regularExpressionWithPattern:@"^\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d+-\\d+\\s\\S+\\[\\d+:\\d+\\]\\s+"
-                                      options:NSRegularExpressionAnchorsMatchLines error:nil];
-    
-    if (output == nil)
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        remove = [NSRegularExpression regularExpressionWithPattern:@"^\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\.\\d+[-\\d\\s]+\\S+\\[\\d+:\\d+\\]\\s+"
+                                                           options:NSRegularExpressionAnchorsMatchLines error:nil];
         output = [NSMutableString new];
+    });
     
     text = [remove stringByReplacingMatchesInString:text options:0 range:NSMakeRange(0, text.length) withTemplate:@""];
 
