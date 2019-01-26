5f3ae402c1ffae86a328485e7b21f3ad2a608441
Pwn20wnd <pwn20wndstuff@gmail.com>
2019-01-26 20:15:06 +0300

    Make SamG's fix actually make sense

--- Undecimus/source/JailbreakViewController.m
+++ Undecimus/source/JailbreakViewController.m
@@ -1926,7 +1926,7 @@ void exploit()
     if ([[NSUserDefaults standardUserDefaults] boolForKey:K_HIDE_LOG_WINDOW]) {
         _outputView.hidden = YES;
         _outputView = nil;
-        _goButtonSpacing.constant = 93;
+        _goButtonSpacing.constant += 80;
     }
     sharedController = self;
     bundledResources = bundledResourcesVersion();
