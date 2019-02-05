3d641eba49f2b3faca0f414a7cc7d1ed934b0ead
Unknown <notquanganh9x@null.io>
2019-02-05 21:10:10 +0700

    Read comments in offsets.h to know how to find offsets

--- Undecimus/source/offsets.h
+++ Undecimus/source/offsets.h
@@ -19,11 +19,15 @@ typedef struct {
     char machine[255];
 } t_device;
 
+
+/* https://github.com/cheesecakeufo/saigon-v0rtex/blob/master/saigon/vortex/offsets.m */
+
 typedef struct{
-    kptr_t offset_task_bsd_info;
-    kptr_t offset_ipc_space_is_task;
-    kptr_t offset_task_itk_registered;
+    kptr_t offset_task_bsd_info; // osfmk/kern/task.h: task->bsd_info, or you can get via _get_bsdtask_info:  ldr x0, [x0, #0x368]
+    kptr_t offset_ipc_space_is_task; // osfmk/ipc/ipc_space.h: ipc_space->is_task
+    kptr_t offset_task_itk_registered; // osfmk/kern/task.h: task->itk_registered
     kptr_t offset_vtab_get_external_trap_for_index;
+    // ida
     kptr_t offset_kernel_base;
     kptr_t offset_zone_map;
     kptr_t offset_rop_ldr_x0_x0_0x10;
@@ -33,15 +37,16 @@ typedef struct{
     kptr_t offset_OSSerializer_serialize;
     kptr_t offset_proc_ucred;
     kptr_t offset_kauth_cred_ref;
-    kptr_t offset_chgproccnt;
+    kptr_t offset_chgproccnt; // contains string chgproccnt
     kptr_t offset_kernel_map;
-    kptr_t offset_vm_map_hdr;
-    kptr_t offset_ipc_port_alloc_special;
-    kptr_t offset_ipc_kobject_set;
-    kptr_t offset_sizeof_task;
-    kptr_t offset_ipc_port_make_send;
-    kptr_t offset_realhost;
-    kptr_t offset_realhost_special;
+    //
+    kptr_t offset_vm_map_hdr; // osfmk/vm/vm_map.h: vm_map->hdr
+    kptr_t offset_ipc_port_alloc_special; // 1 function below "\\\"Over-release of port %p send-once right!\\\""
+    kptr_t offset_ipc_kobject_set; // 1 function above _mach_msg_send_from_kernel_proper
+    kptr_t offset_sizeof_task; // sizeof(task)
+    kptr_t offset_ipc_port_make_send; // third one above _ipc_port_release_send
+    kptr_t offset_realhost; // look at _host_get_special_port, below _lck_mtx_lock
+    kptr_t offset_realhost_special; // below realhost
 }t_offsets;
 
 // Initializer
